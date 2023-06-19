024ac31cf7cf9641d89e3bffbea2cf605acbcb3a
Cecote <thiago.cecote@aluno.ufop.edu.br>
2023-06-19 20:43:00 -0300

    Fix: Trocando tipo de variável de inteira para char

--- src/main.c
+++ src/main.c
@@ -2,22 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 
-int welcome();
+void welcome(char *gameType);
+void playerSelection(char *gameType, char *amountPlayers);
 
 int main(){
-    int gameType;
+    char gameType[4], amountPlayers[4];
 
-    gameType = welcome();
+    welcome(gameType);
+    playerSelection(gameType, amountPlayers);
 
+    if(amountPlayers == 1){
+        printf("implementar...\n");
+    }
+    else if(amountPlayers == 2){}
 
     return 0;
 }
 
-int welcome(){
-    int option;
+void welcome(char *gameType){
 
     printf("-Bem vindo ao jogo de Damas em C-\nO que deseja fazer?\n\n1 - Começar um novo jogo\n2 - Continuar um jogo\n\nDigite a opção desejada (Caso deseje encerrar o jogo, basta digitar 'sair' a qualquer momento):\n");
-    scanf("%d", &option);
+    fgets(gameType, sizeof(gameType), stdin);
+    printf("\n");
 
-    return option;
+}
+
+void playerSelection(char *gameType, char *amountPlayers){
+
+    printf("Digite a quantidade de jogadores < 1 / 2 >:\n");
+    fgets(amountPlayers, sizeof(amountPlayers), stdin);
+    printf("\n");
+
 }
